092977a1f5d6dd0d830944993f7060618bb23bf7
myra-suk <myrasanaa17@gmail.com>
2022-12-20 08:55:56 +0000

    Bug fixes

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -7,7 +7,7 @@ def magic_calculation(a, b):
                 raise Exception("Too far")
             else:
                 result += (a ** b) / i
-        except IndexError:
+        except:
             result = b + a
             break
     return (result)
