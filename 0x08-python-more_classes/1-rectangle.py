cf9a2bbc25df5ae21ba2e73d741935baad3b14cf
myra-suk <myrasanaa17@gmail.com>
2023-01-05 20:16:46 +0000

    Code fixes

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -16,42 +16,37 @@ class Rectangle:
         Args:
             width: The width of the rectangle
             height: The width of the rectangle
-
-
         """
         self.width = width
         self.height = height
 
     @property
     def width(self):
-        """ Method that returns the value of the width of the rectangle
-
-        Returns:
-            The width of the rectangle
-
-
-        """
-
-        return self.__width
+        """ Method that returns the value of the width of the rectangle"""
+        return (self.__width)
 
     @width.setter
     def width(self, value):
+        """Gets the attributes of the rectangle
+
+        Returns:
+            No returns
+
+        Raises:
+        TypeError: If the width is not an integer
+        ValueError: If the value of the width is less than 0
+        """
+
         if not isinstance(value, int):
             raise TypeError("Width must be of integral value")
-        if value < 0:
+        elif value < 0:
             raise ValueError("Width must be greater than or equal to 0")
         self.__width = value
 
     @property
     def height(self):
-        """ Method that returns the height of the rectangle
-
-        Returns:
-            The height of the rectangle
-
-
-        """
-        return self.__height
+        """ Method that returns the height of the rectangle"""
+        return (self.__height)
 
     @height.setter
     def height(self, value):
