5d2fd8dd390104f4c671f4084476b8a275befd18
myra-suk <myrasanaa17@gmail.com>
2023-01-05 23:34:19 +0000

    Fixed the code

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -61,7 +61,7 @@ class Rectangle:
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
-            raise ValueError("height must be >=0")
+            raise ValueError("height must be >= 0")
         self.__height
 
     def area(self):
