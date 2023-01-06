873d24249a57e3991ac68952dd34672b64dc7d0f
myra-suk <myrasanaa17@gmail.com>
2023-01-06 03:04:32 +0000

    Initial commit

---
+++ 0x08-python-more_classes/101-nqueens.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+import sys
+
+
+def init_board(n):
+    """Initialize an `n`x`n` sized chessboard with 0's."""
+    board = []
+    [board.append([]) for i in range(n)]
+    [row.append(' ') for i in range(n) for row in board]
+    return (board)
