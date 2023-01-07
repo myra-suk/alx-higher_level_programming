9872171275dbfd44c63b67658f6b4b0e7d8fae68
myra-suk <myrasanaa17@gmail.com>
2023-01-07 23:20:00 +0000

    Initial Commit

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+"""Defines a funtion that prints the first name and last name"""
+
+
+def say_my_name(first_name, last_name="")
+
+
+"""
+Print a name
+
+Returns:
+    No returns
+i
+Raises:
+    TypeError: If either first_name or last_name are not string values
+"""
+if not isinstance(first_name, str):
+    raise TypeError("first_name must be a string")
+if not isinstance(last_name, str):
+    raise TypeError("last_name ust be a string")
+print("My name is {} {}".format(first_name, last_name))
