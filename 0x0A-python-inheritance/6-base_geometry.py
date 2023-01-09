21cfb5d49d60d8413892be6bf467d0fb22220b79
myra-suk <myrasanaa17@gmail.com>
2023-01-09 19:36:51 +0000

    Initial commit

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""Defines Geometry"""
+
+
+class BaseGeometry:
+    """Defines an empty class BaseGeometry"""
+
+    def area(self):
+        """Not Implemented"""
+        raise Exception("area() is not implemented")
