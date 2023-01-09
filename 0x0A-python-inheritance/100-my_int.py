3a99b41379268af3ba490e91d8dccbcb24289bf1
myra-suk <myrasanaa17@gmail.com>
2023-01-09 22:02:01 +0000

    First commit

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""Defines a class MyInt that inherits from int"""
+
+
+class MyInt(int):
+    """ Class that inherits from class int"""
+
+    def __eq__(self, other):
+        """ Method that returns != check """
+        return int.__ne__(self, other)
+
+    def __ne__(self, other):
+        """ Method that returns == check """
+        return int.__eq__(self, other)
