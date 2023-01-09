84df913cd294db8d4478d25a937fd807aa80d480
myra-suk <myrasanaa17@gmail.com>
2023-01-09 18:25:53 +0000

    Initial commit

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def lookup(obj):
+    """Function that returns the list of the availale attributes
+       and methods of an object
+
+    Args:
+        obj: The instance of the class
+
+    Returns:
+        List of attributes
+    """
+
+    return dir(obj)
