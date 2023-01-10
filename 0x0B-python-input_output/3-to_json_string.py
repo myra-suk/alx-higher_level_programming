6339bd859cd08b1da9948258f7864a077d02e5ec
myra-suk <myrasanaa17@gmail.com>
2023-01-10 21:18:03 +0000

    JSON representation

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""Defines a function that returns the JSON representation of an object"""
+
+import json
+
+
+def to_json_string(my_obj):
+    """Function that returns the JSON representation of an object
+
+    Args:
+        my_obj: object
+
+    Raises:
+        Exception: when the object can't be encoded
+    """
+    return json.dumps(my_obj)
