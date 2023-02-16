3d408265c0f5140b518f8be0409469fd789ee393
myra-suk <myrasanaa17@gmail.com>
2023-02-16 23:51:50 +0000

    Lists all shows by their rating

---
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -0,0 +1,7 @@
+-- Lists all shows from the database by their rating and the records are arranged by descending rating
+SELECT `title`, SUM(`rate`) AS `rating`
+  FROM `tv_shows` AS t
+       INNER JOIN `tv_show_ratings` AS r
+       ON t.`id` = r.`show_id`
+ GROUP BY `title`
+ ORDER BY `rating` DESC;
