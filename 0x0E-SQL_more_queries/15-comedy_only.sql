16c88e9f300d4ca142cf9c326579cbce684b31a8
myra-suk <myrasanaa17@gmail.com>
2023-02-16 23:32:09 +0000

    Lists all comedy shows in the database

---
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,10 @@
+-- Script that lists all comedy shows in the database and records are ordered by descending show title.
+SELECT t.`title`
+  FROM `tv_shows` AS t
+       INNER JOIN `tv_show_genres` AS s
+       ON t.`id` = s.`show_id`
+
+       INNER JOIN `tv_genres` AS g
+       ON g.`id` = s.`genre_id`
+       WHERE g.`name` = "Comedy"
+ ORDER BY t.`title`;
