4c9f2fc8183cd85397483d0a5e1b86d284845b59
myra-suk <myrasanaa17@gmail.com>
2023-02-16 23:54:09 +0000

    Lists all genres in the database by their rating

---
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -0,0 +1,10 @@
+-- Script that lists all genres in the database by the rating and records are arranged by descending order
+SELECT `name`, SUM(`rate`) AS `rating`
+  FROM `tv_genres` AS g
+       INNER JOIN `tv_show_genres` AS s
+       ON s.`genre_id` = g.`id`
+
+       INNER JOIN `tv_show_ratings` AS r
+       ON r.`show_id` = s.`show_id`
+ GROUP BY `name`
+ ORDER BY `rating` DESC;
