196b62f19ce703f59d88df4dd0c00fbbcee7b4c9
myra-suk <myrasanaa17@gmail.com>
2023-02-16 23:14:48 +0000

    Displays NULL for shows without genres

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,6 @@
+-- Script that lists all shows contained in the database hbtn_0d_tvshows, displays NULL for shows without genres and reords are arranged by ascending tv_shows.title and tv_show_genres.genre_id.
+SELECT s.`title`, g.`genre_id`
+  FROM `tv_shows` AS s
+       LEFT JOIN `tv_show_genres` AS g
+       ON s.`id` = g.`show_id`
+ ORDER BY s.`title`, g.`genre_id`;
