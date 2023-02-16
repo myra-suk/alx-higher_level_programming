43d3e1f086a0ff5619b1614afcded67251d6e525
myra-suk <myrasanaa17@gmail.com>
2023-02-16 16:17:36 +0000

    Updates Bob's score to 10

---
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -0,0 +1,2 @@
+-- Script that updates Bob's score to 10 using his name in the table second_table in the database hbtn_0c_0 in the MySQL server 
+UPDATE second_table SET score = 10 WHERE name = 'Bob';
