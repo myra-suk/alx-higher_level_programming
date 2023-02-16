ba0c0e33a3ae9a5cdfd88c743dbfa898753ae97c
myra-suk <myrasanaa17@gmail.com>
2023-02-16 23:39:30 +0000

    Lists all shows and genres linked to the show

---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,9 @@
+-- Script that lists all shows and genres linked to the show from the database and records are arranged by ascending show title and genre name.
+SELECT t.`title`, g.`name`
+  FROM `tv_shows` AS t
+       LEFT JOIN `tv_show_genres` AS s
+       ON t.`id` = s.`show_id`
+
+       LEFT JOIN `tv_genres` AS g
+       ON s.`genre_id` = g.`id`
+ ORDER BY t.`title`, g.`name`;
