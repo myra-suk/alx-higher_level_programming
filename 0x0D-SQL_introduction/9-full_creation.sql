ad1e6241fa9692d47d39dddce540a6e5113f3592
myra-suk <myrasanaa17@gmail.com>
2023-02-16 15:47:46 +0000

    Creates second table and inserts values

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,6 @@
+-- Script that creates a second table second_table in the hbtn_0c_0 database and inserts values into the table
+CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
+INSERT INTO second_table VALUES (1, 'John', 10);
+INSERT INTO second_table VALUES (2, 'Alex', 3);
+INSERT INTO second_table VALUES (3, 'Bob', 14);
+INSERT INTO second_table VALUES (4, 'George', 8);
