768d046fbbec17503c9e666d81a6bc7b77f5b3f5
myra-suk <myrasanaa17@gmail.com>
2023-02-16 21:33:00 +0000

    Creates database hbtn_0d_usa and table states

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,7 +1,7 @@
 -- Script that creates the database hbtn_0d_usa and the table states within the database in the MySQL server
 CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
-CREATE TABLE IF NOT EXISTS `hbtn_0d_usa` `states` (
-   PRIMARY KEY(`id`),
+CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`states` (
+    PRIMARY KEY(`id`),
     `id`   INT          NOT NULL AUTO_INCREMENT,
     `name` VARCHAR(256) NOT NULL
 );
