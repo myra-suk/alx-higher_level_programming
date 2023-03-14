df87b50b707d457e7da847e6ec7efbe1291900d4
myra-suk <myrasanaa17@gmail.com>
2023-03-14 21:36:14 +0000

    Multi languages loop

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
-const myarray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-for (let i = 0; i < 3; i++) {
-	  console.log(myarray[i]);
+let myArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (let i = 0, len = myArray.length; i < len; i++) {
+	  console.log(myArray[i]);
 }
