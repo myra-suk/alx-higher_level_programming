4f732bbed9a10dfd09be27996072d41f5ae39a4c
myra-suk <112114209+myra-suk@users.noreply.github.com>
2023-03-16 01:01:39 +0300

    Update 3-rectangle.js

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-// Script class Rectangle that defines a rectangle
+// class Rectangle that defines a rectangle
 
-const Rectangle = class Rectangle {
+class Rectangle {
   constructor (w, h) {
     if (w > 0 && h > 0) {
       this.width = w;
@@ -10,15 +10,27 @@ const Rectangle = class Rectangle {
   }
 
   print () {
-    let prints = '';
-    for (let cont1 = 0; cont1 < this.height; cont1++) {
-      for (let cont = 0; cont < this.width; cont++) {
-        prints = prints + 'X';
+    let i, j, string;
+    for (i = 0; i < this.height; i++) {
+      string = '';
+      for (j = 0; j < this.width; j++) {
+        string += 'X';
       }
-      console.log(prints);
-      prints = '';
+      console.log(string);
     }
   }
-};
+
+  rotate () {
+    let temp;
+    temp = this.height;
+    this.height = this.width;
+    this.width = temp;
+  }
+
+  double () {
+    this.height *= 2;
+    this.width *= 2;
+  }
+}
 
 module.exports = Rectangle;
