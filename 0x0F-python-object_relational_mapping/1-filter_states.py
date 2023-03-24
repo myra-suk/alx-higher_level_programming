4cba8f78251acd91a0cdd3e537a72527c9dca285
myra-suk <112114209+myra-suk@users.noreply.github.com>
2023-03-24 16:22:32 +0300

    Update 1-filter_states.py

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
-"""
-Lists all values in the states tables of a database where name
-matches the argument
-"""
-import sys
+
+
 import MySQLdb
+from sys import argv
 
-if __name__ == '__main__':
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
-                         db=sys.argv[3], port=3306)
-
-    cur = db.cursor()
-    cur.execute("SELECT * \
-    FROM states \
-    WHERE CONVERT(`name` USING Latin1) \
-    COLLATE Latin1_General_CS = '{}';".format(sys.argv[4]))
-    states = cur.fetchall()
-
-    for state in states:
-        print(state)
+'''
+Script that lists all states from the database
+'''
+if __name__ == "__main__":
+    cont = MySQLdb.connect(
+        host="localhost", port=3306, user=argv[1],
+        password=argv[2], database=argv[3])
+    cursor = cont.cursor()
+    cursor.execute(
+            "SELECT * FROM states WHERE name LIKE"
+            " '{:s}' ORDER BY id ASC".format(argv[4]))
+    db = cursor.fetchall()
+    for i in db:
+        if i[1] == argv[4]:
+            print(i)
