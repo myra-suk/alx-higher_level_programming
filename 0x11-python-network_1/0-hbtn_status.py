bb50edd5db654e2e27c545587ea4d8e8a6d00b94
myra-suk <112114209+myra-suk@users.noreply.github.com>
2023-04-02 00:36:48 +0300

    Update 0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """A script that
-fetches https://intranet.hbtn.io/status.
+fetches https://alx-intranet.hbtn.io/status.
 """
 
 import urllib.request
@@ -8,7 +8,7 @@ import urllib.request
 if __name__ == '__main__':
     import urllib.request
 
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as resp:
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as resp:
         content = resp.read()
         print("Body response:")
         print("\t- type: {}".format(type(content)))
