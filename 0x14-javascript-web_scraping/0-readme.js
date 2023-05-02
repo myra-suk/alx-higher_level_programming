1bbee1cce0d7b8d0639ec51a9b9c6d764e0c66a4
myra-suk <112114209+myra-suk@users.noreply.github.com>
2023-05-02 09:12:58 +0300

    Update 0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,13 +1,11 @@
 #!/usr/bin/node
 // A script that reads and prints the content of a file
 
-const file = process.argv[2];
 const fs = require('fs');
-
-fs.readFile(file, 'utf8', (error, data) => {
-  if (error) {
-    console.log(error);
+fs.readFile(process.argv[2], 'utf8', function (err, contents) {
+  if (contents === undefined) {
+    console.log(err);
   } else {
-    console.log(data);
+    console.log(contents);
   }
 });
